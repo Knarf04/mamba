0ba2a3de3044bc025774fdfb6ca0f83ce9179436
Haochen Shen <haochenshen@mac.watson.ibm.com>
2025-07-15 09:32:22 -0400

    upi mask added

--- mamba_ssm/analysis/upi.py
+++ mamba_ssm/analysis/upi.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torch.nn.functional as F
 
@@ -11,7 +13,7 @@ def scale_dt(scale_mask, dt, dt_bias):
     return (torch.log(y) - dt_bias) / dt
 
 # Easiest way to load/modify the mask: just change the state dict
-def add_upi_to_ckpt(ckpt_dir, upi_dir, save_dir):
+def add_upi_to_state_dict(ckpt_dir, upi_dir, save_dir):
     state_dict = torch.load(ckpt_dir)
     upi_mask_dict = torch.load(upi_dir)
     for i in range(32):
@@ -20,8 +22,20 @@ def add_upi_to_ckpt(ckpt_dir, upi_dir, save_dir):
     torch.save(state_dict, save_dir)
 
 if __name__ == "__main__":
-    add_upi_to_ckpt(
-        ckpt_dir="/gpfs/davis/granites/bamba-merged/consolidated_ckpt.pth", 
-        upi_dir="/gpfs/hshen/UPI_configs/upi_mask_layer.pt", 
-        save_dir="/gpfs/hshen/bamba_upi_tune/bambav2_layer.pth",
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ckpt-dir', type=str, required=True)
+    parser.add_argument('--upi-dir', type=str, required=True)
+    parser.add_argument('--save-dir', type=str, required=True)
+    args = parser.parse_args()
+
+    add_upi_to_state_dict(
+        ckpt_dir=args.ckpt_dir, 
+        upi_dir=args.upi_dir, 
+        save_dir=args.save_dir,
     )
+
+    # add_upi_to_state_dict(
+    #     ckpt_dir="/gpfs/davis/granites/bamba-merged/consolidated_ckpt.pth", 
+    #     upi_dir="/gpfs/hshen/UPI_configs/upi_mask_layer.pt", 
+    #     save_dir="/gpfs/hshen/bamba_upi_tune/bambav2_layer.pth",
+    # )
